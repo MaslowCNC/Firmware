96cf0afe4a1aa039a368a0aceda6a7b2b45099e2
Bar Smith <bar@makesmithtech.com>
2016-07-15 16:44:03 -0700

    using real value

--- cnc_ctrl_v1/GearMotor.cpp
+++ cnc_ctrl_v1/GearMotor.cpp
@@ -61,7 +61,7 @@ void GearMotor::write(int speed){
         
         int pwmFrequency = (speed - 90)*2.8333;
         
-        analogWrite(_pwmPin, 155);
+        analogWrite(_pwmPin, abs(pwmFrequency));
         //Serial.println(abs(pwmFrequency));
         //Serial.println(speed);
         //digitalWrite(_pwmPin, HIGH);
