1add991c594568e63352798b53e23dbb03fa03b7
Bar Smith <bar@makesmithtech.com>
2016-07-27 11:54:04 -0700

    partial cleanup of cnc functions

--- cnc_ctrl_v1/cnc_ctrl_v1.ino
+++ cnc_ctrl_v1/cnc_ctrl_v1.ino
@@ -22,7 +22,6 @@
 
 
 String readString;
-String prependString;
 
 void setup(){
     Serial.begin(19200);
@@ -33,113 +32,6 @@ void setup(){
     xAxis.initializePID();
     yAxis.initializePID();
 }
-
-void interpretCommandString(String readString){
-    int i = 0;
-    char sect[22];
-    
-    while (i < 23){
-        sect[i] = ' ';
-        i++;
-    }
-    
-    if(readString.substring(0, 3) == "G00" || readString.substring(0, 3) == "G01" || readString.substring(0, 3) == "G02" || readString.substring(0, 3) == "G03" || readString.substring(0, 2) == "G0" || readString.substring(0, 2) == "G1" || readString.substring(0, 2) == "G2" || readString.substring(0, 2) == "G3"){
-        prependString = readString.substring(0, 3);
-        prependString = prependString + " ";
-    }
-    
-    if(readString[0] == 'X' || readString[0] == 'Y' || readString[0] == 'Z'){
-        readString = prependString + readString;
-        //Serial.print("prepended: ");
-        //Serial.println(prependString);
-    }
-    
-    if(readString.length() > 0){
-        Serial.println(readString);
-        
-        servoDetachFlag = 0;
-    }
-    
-    if(readString.substring(0, 3) == "G01" || readString.substring(0, 3) == "G00" || readString.substring(0, 3) == "G0 " || readString.substring(0, 3) == "G1 "){
-        G1(readString);
-        Serial.println("ready");
-        Serial.println("gready");
-        readString = "";
-    }
-    
-    if(readString.substring(0, 3) == "G02" || readString.substring(0, 3) == "G2 "){
-        //Serial.println("G02 recognized");
-        G2(readString);
-        Serial.println("ready");
-        Serial.println("gready");
-        readString = "";
-    }
-    
-    if(readString.substring(0, 3) == "G03" || readString.substring(0, 3) == "G3 "){
-        //Serial.println("G03 recognized");
-        G2(readString);
-        Serial.println("ready");
-        Serial.println("gready");
-        readString = "";
-    }
-    
-    if(readString.substring(0, 3) == "G10"){
-        G10(readString);
-        Serial.println("gready");
-        readString = "";
-    }
-    
-    if(readString.substring(0, 3) == "G17"){ //XY plane is the default so no action is taken
-        Serial.println("gready");
-        readString = "";
-    }
-    
-    if(readString.substring(0, 3) == "G20"){
-        //Serial.println("Inches Set");
-        unitScalar = 1; //there are 20 rotations per inch
-        Serial.println("gready");
-        readString = "";
-    }
-    
-    if(readString.substring(0, 3) == "G21"){
-        //Serial.println("mm set");
-        unitScalar = 20; //the machine moves 1.27 mm per rotation
-        Serial.println("gready");
-        readString = "";
-    }
-    
-    if(readString.substring(0, 3) == "G90"){ //G90 is the default so no action is taken
-        Serial.println("gready");
-        readString = "";
-    }
-    
-    if(readString.substring(0, 3) == "M06"){ //Tool change are default so no action is taken
-        Serial.println("gready");
-        readString = "";
-    }
-    
-    
-    if(readString.substring(0, 3) == "B05"){
-        Serial.println("Firmware Version .59");
-        readString = "";
-        Serial.println("gready");
-    }
-    
-    if((readString[0] == 'T' || readString[0] == 't') && readString[1] != 'e'){
-        if(readString[1] != '1'){
-            Serial.print("Please insert tool ");
-            Serial.println(readString);
-            Serial.println("gready");
-        }
-        readString = "";
-    }
-    
-    if (readString.length() > 0){
-        Serial.println(readString);
-        readString = "";
-        Serial.println("gready");
-    }
-} 
 
 void loop(){
     readString = "";
