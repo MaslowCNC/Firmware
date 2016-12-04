dca901334cc68e4785447850269d6cf9812ce483
Bar Smith <bar@makesmithtech.com>
2016-12-04 11:38:24 -0800

    didn't commit main file

--- cnc_ctrl_v1/libraries/Axis/axis.h
+++ cnc_ctrl_v1/libraries/Axis/axis.h
@@ -60,6 +60,7 @@
             int        _previousAngle;
             double     _timeLastMoved;
             double     _pidSetpoint, _pidInput, _pidOutput;
+            double     _Kp=9700, _KiClose=250, _KiMid = 50, _KiFar = 0, _Kd=110;
             PID        _pidController;
             int        _eepromAdr;
             float      _mmPerRotation;
