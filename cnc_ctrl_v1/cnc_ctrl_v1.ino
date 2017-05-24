134494c4d6be8fc2dc76c0a31fc932e17d0cd098
Barbour Smith <bar@makesmithtech.com>
2017-05-24 11:21:45 -0700

    Remove testing code

--- cnc_ctrl_v1/cnc_ctrl_v1.ino
+++ cnc_ctrl_v1/cnc_ctrl_v1.ino
@@ -30,14 +30,14 @@ void setup(){
     
     Serial.println("Grbl v1.00");
     
-    int i = 0;
+    /*int i = 0;
     String testGcode = "G20 \nG1X-1 Y-1 F100 \nG1 X1 \nG3 Y2 J1.5\nG1 X-1 \nG1 Y-1\nG1 X0 Y0\n";
     
     Serial.println(testGcode.length());
     while (i < testGcode.length()){
         ringBuffer.write(testGcode[i]);
         i++;
-    }
+    }*/
     
 }
 
