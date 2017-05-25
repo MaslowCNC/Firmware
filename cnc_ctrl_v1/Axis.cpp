7eeda5a036762a44f89fc9f23e4c6850139dfba7
Barbour Smith <bar@makesmithtech.com>
2017-05-25 10:01:39 -0700

    Strip out old code related to motor calibration

--- cnc_ctrl_v1/Axis.cpp
+++ cnc_ctrl_v1/Axis.cpp
@@ -46,8 +46,6 @@ motorGearboxEncoder(pwmPin, directionPin1, directionPin2, encoderPin1, encoderPi
         set(_readFloat(_eepromAdr + SIZEOFFLOAT));
     }
     
-    _readAllLinSegs(_eepromAdr);
-    
     initializePID();
     
     motorGearboxEncoder.setName(_axisName);
@@ -139,10 +137,8 @@ void   Axis::changeEncoderResolution(int newResolution){
 int    Axis::detach(){
     
     if (motorGearboxEncoder.motor.attached()){
-        _writeFloat (_eepromAdr+SIZEOFFLOAT, read());
+        _writeFloat (_eepromAdr+SIZEOFFLOAT, read());      //Store the axis position
         EEPROM.write(_eepromAdr, EEPROMVALIDDATA);
-        
-        _writeAllLinSegs(_eepromAdr);
         
     }
     
@@ -215,66 +211,6 @@ void   Axis::_writeFloat(unsigned int addr, float x){
     }
 }
 
-void   Axis::_writeLinSeg(unsigned int addr, LinSegment linSeg){
-    
-    //flag that data is good
-    EEPROM.write(addr, EEPROMVALIDDATA);
-    
-    _writeFloat(addr + 1                , linSeg.slope);
-    _writeFloat(addr + 1 + 1*SIZEOFFLOAT, linSeg.intercept);
-    _writeFloat(addr + 1 + 2*SIZEOFFLOAT, linSeg.positiveBound);
-    _writeFloat(addr + 1 + 3*SIZEOFFLOAT, linSeg.negativeBound);
-}
-
-void   Axis::_writeAllLinSegs(unsigned int addr){
-    /*
-    Write all of the LinSegment objects which define the motors response into the EEPROM
-    */
-    
-    addr = addr + 2*SIZEOFFLOAT;
-    
-    //Write into memory
-    _writeLinSeg(addr                 , motorGearboxEncoder.motor.getSegment(0));
-    _writeLinSeg(addr + 1*SIZEOFLINSEG, motorGearboxEncoder.motor.getSegment(1));
-    _writeLinSeg(addr + 2*SIZEOFLINSEG, motorGearboxEncoder.motor.getSegment(2));
-    _writeLinSeg(addr + 3*SIZEOFLINSEG, motorGearboxEncoder.motor.getSegment(3));
-    
-}
-
-LinSegment   Axis::_readLinSeg(unsigned int addr){
-    /*
-    Read a LinSegment object from the EEPROM
-    */
-    
-    LinSegment linSeg;
-    
-    //check that data is good
-    if (EEPROM.read(addr) == EEPROMVALIDDATA){
-        linSeg.slope         =  _readFloat(addr + 1                );
-        linSeg.intercept     =  _readFloat(addr + 1 + 1*SIZEOFFLOAT);
-        linSeg.positiveBound =  _readFloat(addr + 1 + 2*SIZEOFFLOAT);
-        linSeg.negativeBound =  _readFloat(addr + 1 + 3*SIZEOFFLOAT);
-    }
-    
-    return linSeg;
-}
-
-void   Axis::_readAllLinSegs(unsigned int addr){
-   /*
-   Read back all the LinSegment objects which define the motors behavior from the EEPROM
-   */
-    
-    addr = addr + 8;
-    
-    LinSegment linSeg;
-    
-    for (int i = 0; i < 4; i++){
-        linSeg = _readLinSeg (addr + i*SIZEOFLINSEG);
-        
-        motorGearboxEncoder.motor.setSegment(i, linSeg.slope, linSeg.intercept,  linSeg.negativeBound, linSeg.positiveBound);
-    }
-}
-
 void   Axis::wipeEEPROM(){
     /*
     
@@ -318,7 +254,6 @@ int    Axis::_detectDirectionChange(float _pidSetpoint){
     
     return retVal;
 }
-
 
 void   Axis::test(){
     /*
@@ -373,233 +308,4 @@ void   Axis::test(){
     //stop the motor
     motorGearboxEncoder.motor.directWrite(0);
     Serial.print("<Idle,MPos:0,0,0,WPos:0.000,0.000,0.000>");
-}
-
-void   Axis::computeMotorResponse(){
-    
-    //remove whatever transform is applied
-    motorGearboxEncoder.motor.setSegment(0 , 1, 0, 0, 0);
-    motorGearboxEncoder.motor.setSegment(1 , 1, 0, 0, 0);
-    motorGearboxEncoder.motor.setSegment(2 , 1, 0, 0, 0);
-    motorGearboxEncoder.motor.setSegment(3 , 1, 0, 0, 0);
-    
-    //In the positive direction
-    //-----------------------------------------------------------------------------------
-    
-    float scale = 255/measureMotorSpeed(255); //Y3*scale = 255 -> scale = 255/Y3
-    
-    int stallPoint;
-    float motorSpeed;
-    
-    int upperBound = 255; //the whole range is valid
-    int lowerBound =   0;
-    
-    while (true){ //until a value is found
-        Serial.print("Testing: ");
-        Serial.println((upperBound + lowerBound)/2);
-        
-        motorSpeed = measureMotorSpeed((upperBound + lowerBound)/2);
-        
-        if (motorSpeed == 0){                               //if the motor stalled
-            lowerBound = (upperBound + lowerBound)/2;           //shift lower bound to be the guess
-            Serial.println("- stall");
-        }
-        else{                                               //if the motor didn't stall
-            upperBound = (upperBound + lowerBound)/2;           //shift upper bound to be the guess
-            Serial.println("- good");
-        }
-        
-        if (upperBound - lowerBound <= 1){                  //when we've converged on the first point which doesn't stall
-            break;                                              //exit loop
-        }
-    }
-    
-    stallPoint = upperBound;
-    
-    Serial.print("decided on final value of: ");
-    Serial.println(stallPoint);
-    
-    //compute a model of the motor from the given data points
-    float X1 = stallPoint;
-    float Y1 = scale*motorSpeed;
-    
-    float X2 = (255 - X1)/2;
-    float Y2 = scale*measureMotorSpeed(X2);
-    
-    float X3 = 255;
-    float Y3 = 255;
-    
-    float M1 = (Y1 - Y2)/(X1 - X2);
-    float M2 = (Y2 - Y3)/(X2 - X3);
-    
-    float I1 = -1*(Y1 - (M1*X1));
-    float I2 = -1*(Y2 - (M2*X2));
-    
-    
-    //Apply the model to the motor
-    if(abs(stallPoint) == 255){
-        Serial.print("Message: Unable to calibrate ");
-        Serial.print(_axisName);
-        Serial.println(" motor. Please try Test Motors/Encoders");
-    }
-    else{
-        motorGearboxEncoder.motor.setSegment(2 , M1, I1,    0,   Y2);
-        motorGearboxEncoder.motor.setSegment(3 , M2, I2, Y2-1, Y3+1);
-    }
-    
-    //In the negative direction
-    //-----------------------------------------------------------------------------
-    
-    scale = -255/measureMotorSpeed(-255); //Y3*scale = 255 -> scale = 255/Y3
-    
-    upperBound =      0; //the whole range is valid
-    lowerBound =   -255;
-    
-    while (true){ //until a value is found
-        
-        Serial.print("Testing: ");
-        Serial.println((upperBound + lowerBound)/2);
-        
-        motorSpeed = measureMotorSpeed((upperBound + lowerBound)/2);
-        if (motorSpeed == 0){                               //if the motor stalled
-            upperBound = (upperBound + lowerBound)/2;           //shift lower bound to be the guess
-            Serial.println("-stall");
-        }
-        else{                                               //if the motor didn't stall
-            lowerBound = (upperBound + lowerBound)/2;           //shift upper bound to be the guess
-            Serial.println("-good");
-        }
-        
-        if (upperBound - lowerBound <= 1){                  //when we've converged on the first point which doesn't stall
-            break;                                              //exit loop
-        }
-    }
-    
-    stallPoint = lowerBound;
-    
-    Serial.print("decided on a final value of: ");
-    Serial.println(stallPoint);
-    
-    //At this point motorSpeed is the speed in RPM at the value i which is just above the stall speed
-    
-    //Compute a model for the motor's behavior using the given data-points
-    X1 = stallPoint;
-    Y1 = scale*motorSpeed;
-    
-    X2 = (-255 - X1)/3 + X1;
-    Y2 = scale*measureMotorSpeed(X2);
-    
-    X3 = -255;
-    Y3 = -255;
-    
-    M1 = (Y1 - Y2)/(X1 - X2);
-    M2 = (Y2 - Y3)/(X2 - X3);
-    
-    I1 = -1*(Y1 - (M1*X1));
-    I2 = -1*(Y2 - (M2*X2));
-    
-    
-    //Apply the model to the motor
-     if(abs(stallPoint) == 255){
-        Serial.print("Message: Unable to calibrate ");
-        Serial.print(_axisName);
-        Serial.println(" motor. Please try Test Motors/Encoders");
-    }
-    else{
-        motorGearboxEncoder.motor.setSegment(0 , M1, I1,   Y2,    0);
-        motorGearboxEncoder.motor.setSegment(1 , M2, I2, Y3-1, Y2+1);
-    }
-    
-    Serial.println("Calibration complete.");
-    
-}
-
-float  Axis::_speedSinceLastCall(){
-    //static variables to persist between calls
-    static long time = millis();
-    static long prevEncoderValue = motorGearboxEncoder.encoder.read();
-    
-    //compute dist moved
-    int elapsedTime = millis() - time;
-    int distMoved   = motorGearboxEncoder.encoder.read() - prevEncoderValue;
-    float speed = float(distMoved)/float(elapsedTime);
-    
-    //catch if time is zero
-    if (elapsedTime < 10){
-        speed = 0;
-    }
-    
-    //set values for next call
-    time = millis();
-    prevEncoderValue = motorGearboxEncoder.encoder.read();
-    
-    //return the absolute value because speed is not a vector
-    return abs(speed);
-}
-
-float  Axis::measureMotorSpeed(int speed){
-    /*
-    Returns the motors speed in RPM at a given input value
-    */
-    
-    int sign = speed/abs(speed);
-    
-    _disableAxisForTesting = true;
-    attach();
-    
-    int numberOfStepsToTest = 2000;
-    int timeOutMS           = 30*1000; //30 seconds
-    bool stall              = false;
-    
-    //run the motor for numberOfStepsToTest steps positive and record the time taken
-    
-    
-    long originalEncoderPos  = motorGearboxEncoder.encoder.read();
-    long startTime = millis();
-    
-    //until the motor has moved the target distance
-    while (abs(originalEncoderPos - motorGearboxEncoder.encoder.read()) < numberOfStepsToTest){
-        //establish baseline for speed measurement
-        _speedSinceLastCall();
-        
-        //command motor to spin at speed
-        motorGearboxEncoder.motor.write(speed);
-        
-        //wait
-        delay(200);
-        
-        //print to prevent connection timeout
-        Serial.println("<Idle,MPos:0,0,0>");
-        
-        //check to see if motor is moving
-        if (_speedSinceLastCall() < .01){
-            stall = true;
-            break;
-        }
-    }
-    int posTime = millis() - startTime;
-    
-    //rotations = number of steps taken / steps per rotation
-    float rotations = (originalEncoderPos - motorGearboxEncoder.encoder.read())/_encoderSteps;
-    //minutes = time elapsed in ms * 1000ms/s *60 seconds per minute
-    float minutes   = posTime/(1000.0*60.0);
-    
-    //RPM is rotations per minute.
-    float RPM = rotations/minutes;
-    
-    if (stall){
-        RPM = 0;
-    }
-    
-    //move back to start point
-    _disableAxisForTesting = false;
-    _timeLastMoved = millis();
-    for (long startTime = millis(); millis() - startTime < 2000; millis()){
-        hold();
-        delay(50);
-        //print to prevent connection timeout
-        Serial.println("<Idle,MPos:0,0,0>");
-    }
-    
-    return RPM;
 }
