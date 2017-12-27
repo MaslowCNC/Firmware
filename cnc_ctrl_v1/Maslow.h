96fa2fc2ea4f2f6e747a0a4d12b46b49ba62a159
Barbour Smith <bar@makesmithtech.com>
2017-12-27 12:25:52 -0800

    bump version number

--- cnc_ctrl_v1/Maslow.h
+++ cnc_ctrl_v1/Maslow.h
@@ -18,7 +18,7 @@
 #define maslow_h
 
 // Maslow Firmware Version tracking
-#define VERSIONNUMBER 1.011
+#define VERSIONNUMBER 1.02
 
 // Define standard libraries used by maslow.
 #include <avr/io.h>
