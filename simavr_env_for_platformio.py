87ea7a14b3003707653fd2b5ed2e438d0cd61abe
idoc <icarmel@appdynamics.com>
2018-02-20 16:19:25 -0800

    Disable uploading for simavr
    My previous commit created a PlatformIO project that built a customised
    binary for running in simavr. It also attempted to upload this binary
    when an Arduino was plugged in, which was never intended.
    
    This commit disables the uploading for simavr.

--- simavr_env_for_platformio.py
+++ simavr_env_for_platformio.py
@@ -1,12 +1,11 @@
 Import('env')
 
-#
-# Dump build environment (for debug)
-# print env.Dump()
-#
-
+# Run the linker with "-g", to prevent stripping of debugging symbols
 env.Append(
   LINKFLAGS=[
       "-g"
   ]
 )
+
+# Don't try to upload the firmware
+env.Replace(UPLOADHEXCMD="echo Upload is not supported for ${PIOENV}. Skipping")
