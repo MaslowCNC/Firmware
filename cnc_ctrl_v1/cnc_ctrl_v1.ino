02be8124f62ae0799fc6805bcbe8ccab72cc90ba
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-28 15:07:16 -0700

    bump version number

--- cnc_ctrl_v1/cnc_ctrl_v1.ino
+++ cnc_ctrl_v1/cnc_ctrl_v1.ino
@@ -72,7 +72,7 @@ void setup(){
     Timer1.attachInterrupt(runsOnATimer);
     #endif
     
-    Serial.println(F("Grbl v1.00"));  // Why GRBL?  Apparenlty because some programs are silly and look for this as an initailization command
+    Serial.println(F("Grbl v1.00"));  // Why GRBL?  Apparently because some programs are silly and look for this as an initialization command
     Serial.println(F("ready"));
     reportStatusMessage(STATUS_OK);
 }
