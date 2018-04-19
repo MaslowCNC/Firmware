df53ca00cf5c33142e45710a930ab5f9db8528ff
Scott Smith <scottsm@mac.com>
2018-04-19 16:13:23 -0700

    Use pullups to check board version, use #defines for version
    Use pullups to check board version
    use #defines for version
    add two more version pins for future use, pull them up as well

--- cnc_ctrl_v1/System.cpp
+++ cnc_ctrl_v1/System.cpp
@@ -214,7 +214,25 @@ void configAuxLow(int AUX1, int AUX2, int AUX3, int AUX4, int AUX5, int AUX6) {
 
 
 int getPCBVersion(){
-    return (8*digitalRead(53) + 4*digitalRead(52) + 2*digitalRead(23) + 1*digitalRead(22)) - 1;
+    pinMode(VERS1,INPUT_PULLUP);
+    pinMode(VERS2,INPUT_PULLUP);
+    pinMode(VERS3,INPUT_PULLUP);
+    pinMode(VERS4,INPUT_PULLUP);
+    pinMode(VERS5,INPUT_PULLUP);
+    pinMode(VERS6,INPUT_PULLUP);
+    int pinCheck = (32*digitalRead(VERS6) + 16*digitalRead(VERS5) + 8*digitalRead(VERS4) + 4*digitalRead(VERS3) + 2*digitalRead(VERS2) + 1*digitalRead(VERS1));
+    switch (pinCheck) {
+        // boards v1.1, v1.2, v1.3 don't strap VERS3-6 low
+        case B111101: case B111110: case B111111: // v1.1, v1.2, v1.3
+            pinCheck -= B111100; // strip off the unstrapped bits
+            // TLE5206 = false;
+            break;
+        case B110100: // board v1.4 doesn't strap VERS5-6 low
+            pinCheck -= B110000; // strip off the unstrapped bits
+            // TLE5206 = true;
+            break;
+}
+    return pinCheck - 1;
 }
 
 //
