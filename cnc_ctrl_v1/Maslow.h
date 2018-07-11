beae56949c0a0537266a0c5347876f93b7a2ee17
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-07-11 14:09:27 -0700

    bump version number

--- cnc_ctrl_v1/Maslow.h
+++ cnc_ctrl_v1/Maslow.h
@@ -18,7 +18,7 @@
 #define maslow_h
 
 // Maslow Firmware Version tracking
-#define VERSIONNUMBER 1.16
+#define VERSIONNUMBER 1.17
 
 // Define standard libraries used by maslow.
 #include <avr/io.h>
