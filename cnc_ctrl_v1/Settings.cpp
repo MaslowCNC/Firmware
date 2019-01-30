349a4628345439aeecff365950460580a9496aba
C0dePr1sm <c0depr1sm@users.noreply.github.com>
2019-01-29 22:46:26 -0500

    Added sled weight, chain stretch, and two reserved  parameters

--- cnc_ctrl_v1/Settings.cpp
+++ cnc_ctrl_v1/Settings.cpp
@@ -105,6 +105,10 @@ void settingsReset() {
     sysSettings.leftChainTolerance = 0.0;    // float leftChainTolerance;
     sysSettings.rightChainTolerance = 0.0;    // float rightChainTolerance;
     sysSettings.positionErrorLimit = 2.0;  // float positionErrorLimit;
+    sysSettings.reserved1 = 0.0;
+    sysSettings.reserved2 = 0.0;
+    sysSettings.chainElongationFactor = 8.1E-6; // m/m/N
+    sysSettings.sledWeight = 11.6*9.8; // Newtons. My sled has one ring kit, one Rigid 2200 router and two 2.35kg bricks on a 5/8" thick mdf 18" diameter base.
     sysSettings.eepromValidData = EEPROMVALIDDATA; // byte eepromValidData;
 }
 
@@ -405,6 +409,22 @@ byte settingsStoreGlobalSetting(const byte& parameter,const float& value){
               break;
         case 42:
               sysSettings.positionErrorLimit = value;
+              break;
+        case 43:
+              sysSettings.reserved1 = value;
+              kinematics.recomputeGeometry();
+              break;
+        case 44:
+              sysSettings.reserved2 = value;
+              kinematics.recomputeGeometry();
+              break;
+        case 45:
+              sysSettings.chainElongationFactor = value;
+              kinematics.recomputeGeometry();
+              break;
+        case 46:
+              sysSettings.sledWeight = value;
+              kinematics.recomputeGeometry();
               break;
         default:
               return(STATUS_INVALID_STATEMENT);
