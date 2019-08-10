a1b55bd9b7674ae4f7e6e14115f1503d1856d4a6
Scott Smith <scottsm@mac.com>
2019-08-10 10:42:49 -0700

    Update cnc_ctrl_v1.ino
    identify TLE9201 in connection handshake response

--- cnc_ctrl_v1/cnc_ctrl_v1.ino
+++ cnc_ctrl_v1/cnc_ctrl_v1.ino
@@ -31,6 +31,7 @@
  */
     
 
+// TLE9201 version
 // TLE5206 version
 
 #include "Maslow.h"
@@ -58,6 +59,7 @@ void setup(){
     Serial.print(F("PCB v1."));
     Serial.print(getPCBVersion());
     if (TLE5206 == true) { Serial.print(F(" TLE5206 ")); }
+    if (TLE9201 == true) { Serial.print(F(" TLE9201 ")); }
     Serial.println(F(" Detected"));
     sys.inchesToMMConversion = 1;
     sys.writeStepsToEEPROM = false;
