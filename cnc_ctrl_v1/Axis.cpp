d6f100a60d97a60f8490e6c1dd89a3f8d413a720
Gab <info@eastbaysource.com>
2021-06-09 14:57:31 -0400

    Update Axis.cpp

--- cnc_ctrl_v1/Axis.cpp
+++ cnc_ctrl_v1/Axis.cpp
@@ -310,14 +310,12 @@ void   Axis::test(){
     for (int ramp = 0 ; ramp <= 255; ramp += 1) {
     motorGearboxEncoder.motor.directWrite(ramp);
     maslowDelay(2);
-    if (sys.stop){return;}
     }
     maslowDelay(2000);
     //ramp down
     for (int ramp = 255 ; ramp >= 0; ramp += -1) {
     motorGearboxEncoder.motor.directWrite(ramp);
     maslowDelay(2);
-    if (sys.stop){return;}
     }
  
     //check to see if it moved
@@ -335,14 +333,12 @@ void   Axis::test(){
     //move the motor in the other direction
     for (int ramp = 0 ; ramp >= -255; ramp += -1) {
     motorGearboxEncoder.motor.directWrite(ramp);
-    maslowDelay(2);
-    if (sys.stop){return;}  
+    maslowDelay(2);  
     }
     maslowDelay(2000); 
     for (int ramp = -255 ; ramp <= 0; ramp += 1) {
     motorGearboxEncoder.motor.directWrite(ramp);
     maslowDelay(2);
-    if (sys.stop){return;}
     }
     //check to see if it moved
     if(encoderPos - motorGearboxEncoder.encoder.read() < -500){
